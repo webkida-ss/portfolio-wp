590a669b9d2dee00643a26df11f4a31d79d69f66
webkida-ss <webkida.production@gmail.com>
2021-09-30 08:35:40 +0900

    修正中

--- src-php/20_component/03_contact/_contact.php
+++ src-php/20_component/03_contact/_contact.php
@@ -1,3 +1,7 @@
-<div class="">
-	<?php echo do_shortcode('[mwform_formkey key="34"]'); ?>
+<div class="contatct">
+	<h2 class="section-title">Contact</h2>
+	<div class="inner contact__inner">
+
+		<?php echo do_shortcode('[mwform_formkey key="34"]'); ?>
+	</div>
 </div>
