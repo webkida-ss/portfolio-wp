ff03bf24a0a81a844d4bcd2482bffa68c2f81544
webkida-ss <webkida.production@gmail.com>
2021-09-30 00:07:46 +0900

    基本設定修正

--- single-work.php
+++ single-work.php
@@ -1,1 +1,43 @@
 <!-- 制作実績 -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<?php get_header(); ?>
+</head>
+
+<body>
+	<div class="wrapper single-work">
+
+		<!-------------------------------------------------- 
+		- ヘッダー
+		---------------------------------------------------->
+		<?php get_template_part('src-php/10_common/_header'); ?>
+
+
+		<!-------------------------------------------------- 
+		- メイン
+		---------------------------------------------------->
+		<div class="container">
+
+			<!-------------------------- 
+			- メイン
+			---------------------------->
+			<main class="main">
+				<section class="single-work__">
+					<!-- <?php get_template_part('src-php/20_component//'); ?> -->
+				</section>
+			</main>
+
+		</div><!-- /.container -->
+
+		<!-------------------------------------------------- 
+		- フッター
+		---------------------------------------------------->
+		<?php get_template_part('src-php/10_common/_footer'); ?>
+	</div><!-- /.wrapper -->
+
+	<?php get_footer(); ?>
+</body>
+
+</html>
