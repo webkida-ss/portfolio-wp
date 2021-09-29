0e23c3b02dce1c0c64d96cff3a636b29e57ce4e7
webkida-ss <webkida.production@gmail.com>
2021-09-29 22:11:56 +0900

    footer作成

---
+++ src-php/10_common/_footer.php
@@ -0,0 +1,24 @@
+<footer class="footer">
+	<div class="inner footer__inner">
+
+		<div class="footer__foreign-link">
+			<ul>
+				<li><a href="https://icons8.com/icon/20909/html-5">HTML 5 icon by Icons8</a></li>
+				<li><a href="https://icons8.com/icon/QBqFNfPPB2Kx/sass">Sass icon by Icons8</a></li>
+				<li><a href="https://icons8.com/icon/108784/javascript">JavaScript icon by Icons8</a></li>
+				<li><a href="https://icons8.com/icon/13664/ワードプレス">ワードプレス icon by Icons8</a></li>
+				<li><a href="https://icons8.com/icon/49072/googleサイト">Googleサイト icon by Icons8</a></li>
+				<li><a href="https://icons8.com/icon/AZOZNnY73haj/github">GitHub icon by Icons8</a></li>
+				<li><a href="https://icons8.com/icon/20906/git">Git icon by Icons8</a></li>
+				<li><a href="https://icons8.com/icon/60469/twitter-(四角)">Twitter (四角) icon by Icons8</a></li>
+				<li><a href="https://icons8.com/icon/13679/java-coffee-cupのロゴ">Java Coffee Cupのロゴ icon by Icons8</a></li>
+				<li><a href="https://icons8.com/icon/GX4iT6biRXL-/gulp">Gulp icon by Icons8</a></li>
+				<li><a href="https://icons8.com/icon/rY6agKizO9eb/vue-js">Vue Js icon by Icons8</a></li>
+			</ul>
+		</div><!-- /.footer__foreign-link -->
+
+		<!-- コピーライト -->
+		<small class="footer__copyright">© 2021 Shun Sakakida</small>
+
+	</div>
+</footer>
