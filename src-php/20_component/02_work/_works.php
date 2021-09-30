aea0c9643de5bacbf938c26fe2955a5f718e8fbc
webkida-ss <webkida.production@gmail.com>
2021-10-01 00:19:26 +0900

    修正

--- src-php/20_component/02_work/_works.php
+++ src-php/20_component/02_work/_works.php
@@ -1,17 +1,20 @@
+<!-- データ -->
+<?php
+$post_num = isset($args) ? $args : 6; // 表示件数：デフォルト6件
+$args = array(
+	'post_type' => 'work', // 投稿タイプ：制作実績
+	'order' => 'DESC',
+	'posts_per_page' => $post_num,
+);
+$the_query = get_posts($args); // 投稿取得
+?>
+
+<!-- 本文 -->
 <div class="works">
 	<h2 class="section-title">制作実績<span>works</span></h2>
 	<div class="inner works__container">
 
-		<?php
-		$args = array(
-			'post_type' => 'work', // 投稿タイプ：制作実績
-			'order' => 'DESC',
-			'posts_per_page' => 6,
-		);
-		// 投稿取得
-		$the_query = get_posts($args);
-		if ($the_query) :
-		?>
+		<?php if ($the_query) : ?>
 			<ul class="works__list">
 				<?php foreach ($the_query as $post) :
 					setup_postdata($post);
@@ -19,7 +22,7 @@
 				?>
 
 					<li class="works__item">
-						<a href="<?php the_field('work_url'); ?>">
+						<a href="<?php the_permalink(); ?>">
 							<div class="works__img">
 								<?php the_post_thumbnail(); ?>
 							</div>
