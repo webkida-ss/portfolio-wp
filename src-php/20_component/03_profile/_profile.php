c11e8e87bc883e29c209a66fd06e8fb01587ad7b
webkida-ss <webkida.production@gmail.com>
2021-10-02 14:20:45 +0900

    プロフィール作成

--- src-php/20_component/03_profile/_profile.php
+++ src-php/20_component/03_profile/_profile.php
@@ -2,5 +2,32 @@
 	<h2 class="section-title">プロフィール<span>profile</span></h2>
 	<div class="inner profile__container">
 
+		<div class="profile__imgs">
+			<div class="profile__icon"><img src="<?php echo esc_url(get_template_directory_uri() . '/assets/img/profile-icon-transparent.png'); ?>" alt="アイコン"></div>
+			<div class="profile__sns">
+				<a href="https://twitter.com/webkida1" class="wow animated" id="js-twitter">
+					<img src="<?php echo esc_url(get_template_directory_uri() . '/assets/img/sns/twitter.png'); ?>" alt="twitter"></a>
+				<a href="https://github.com/webkida-ss" class="wow" id="js-github">
+					<img src="<?php echo esc_url(get_template_directory_uri() . '/assets/img/sns/github.png'); ?>" alt="github"></a>
+			</div>
+		</div><!-- /.profile__imgs -->
+
+		<div class="profile__context">
+			<p class="profile__name">榊田 峻（さかきだ しゅん）</p>
+			<p class="profile__overview">
+				神奈川県出身、東京都在住<br>
+				2017年 大手SIer 入社 → 2020年 Web系ベンチャー 入社
+			</p>
+			<p class="profile__intro">
+				東京を拠点にWeb制作フリーランスとして活動しています。<br class="hidden-sp">
+				本業がJavaという言語のWebアプリ開発エンジニアのため、<br>
+				<span class="profile__bullets">ITに精通していること</span><br>
+				<span class="profile__bullets">保守性の高いコーディングが可能であること</span><br>
+				<span class="profile__bullets">技術的なキャッチアップが早いこと</span><br>
+				が強みです。<br>
+				フルリモートで働くことに慣れており、素早いレスポンスを心がけています。<br>
+			</p>
+		</div><!-- /.profile__context -->
+
 	</div>
 </div>
