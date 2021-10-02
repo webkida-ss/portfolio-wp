e8ae4b5f2a902381d788866d9d7ff88d57059997
webkida-ss <webkida.production@gmail.com>
2021-10-02 14:42:12 +0900

    修正

--- src-php/20_component/03_profile/_skill.php
+++ src-php/20_component/03_profile/_skill.php
@@ -1,9 +1,9 @@
 <!-- データ -->
 <?php
-$array = [
+$array_skill = [
 	["HTML", "HTML", "html.png"],
 	["Sass", "Sass <br> (CSS)", "sass.png"],
-	["JavaScript", "JavaScript<br>(jQuery)", ".png"],
+	["JavaScript", "JavaScript<br>(jQuery)", "javascript.png"],
 	["WordPress", "WordPress", "wordpress.png"],
 	["Git", "Git", "git.png"],
 	["gulp", "gulp", "gulp.png"],
@@ -19,8 +19,15 @@ $array = [
 	<h2 class="section-title">スキル<span>skill</span></h2>
 	<div class="inner skill__container">
 
-		<li class="skill__list">
+		<ul class="skill__list">
+			<?php foreach ($array_skill as $item) : ?>
+				<li class="skill__item">
+					<div class="skill__item--img">
+						<img src="<?php echo esc_url(get_template_directory_uri() . '/assets/img/skill/' . $item[2]); ?>" alt="<?php echo $item[0]; ?>">
+					</div>
+				</li>
+			<?php endforeach; ?>
+		</ul>
 
-		</li>
 	</div>
 </div>
