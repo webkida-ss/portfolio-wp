62ebf8deb9afe548c8c3b4eb011e18776b446d2e
webkida-ss <webkida.production@gmail.com>
2021-10-02 20:45:27 +0900

    修正

--- src-php/20_component/01_top/_service.php
+++ src-php/20_component/01_top/_service.php
@@ -1,6 +1,22 @@
+<!-- データ -->
+<?php
+$arrat = [];
+?>
+
+<!-- 本文 -->
 <div class="service">
 	<h2 class="section-title">サービス<span>service</span></h2>
 	<div class="inner service__container">
+		<div class="service__lead">
+			Webサイト構築からMEO対策、SNS運用などWeb関連のお仕事なんでもまずはご相談ください！<br>
+			コーディング・デザインに加え、 サーバ契約からサイト公開・運用サポートまで一括で承ります。<br>
+			既存サイトの改修 / WordPress化、 新規のLP / HP制作、WordPress構築いずれも対応可能です。<br>
+			ご気軽にご相談ください！
+		</div>
+		<ul class="service__list">
+			<li class="service__item">
 
+			</li>
+		</ul>
 	</div>
 </div>
