d750d19a8ff26d5ac155cb806940e52b6cb12b1c
webkida-ss <webkida.production@gmail.com>
2021-10-02 14:33:22 +0900

    修正

--- src-php/20_component/03_profile/_skill.php
+++ src-php/20_component/03_profile/_skill.php
@@ -1,6 +1,26 @@
+<!-- データ -->
+<?php
+$array = [
+	["HTML","HTML","html.png"],
+	["Sass","Sass <br> (CSS)","sass.png"],
+	["JavaScript","JavaScript<br>(jQuery)",".png"],
+	["WordPress","WordPress","wordpress.png"],
+	["Git","Git","git.png"],
+	["gulp","gulp","gulp.png"],
+	["Java","Java<br>(Spring)","java.png"],
+	["Vue","Vue","vue.png"],
+	["","",".png"],
+	["","",".png"],
+];
+?>
+
+<!-- 本文 -->
 <div class="skill">
 	<h2 class="section-title">スキル<span>skill</span></h2>
 	<div class="inner skill__container">
 
+		<li class="skill__list">
+
+		</li>
 	</div>
 </div>
